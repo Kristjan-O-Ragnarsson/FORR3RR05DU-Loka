13c6d55c1e2e53426755da1a01b9822710252d99
Kristjan-O-Ragnarsson <kristjanra262@nemi.tskoli.is>
2019-04-22 02:46:42 +0000

    added Python

---
+++ Python/test.py
@@ -0,0 +1,13 @@
+"""
+Made By Kristjan O. Ragnarsson
+github.com/Kristjan-O-Ragnarsson
+"""
+from sys import stdout, stdin, stderr
+
+write, readln, err = stdout.write, stdin.readline, stderr.write
+
+S, I, F, N, W = str, int, float, '\n', ' '
+
+
+if __name__ == '__main__':
+    write("Test" + N)
